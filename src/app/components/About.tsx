cccca368586a9d21860ca2c1c3fbbe45708802a9
yonathanth <yonathantenaye@gmail.com>
2025-03-21 00:35:49 +0300

    Refactor About and Navbar components for improved readability and formatting

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,14 +1,17 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Tech from '../../../public/Group 31.svg'
-import Business from '../../../public/Group 32.svg'
-import Gears from '../../../public/Group 33.svg'
+import Image from "next/image";
+import Tech from "../../../public/Group 31.svg";
+import Business from "../../../public/Group 32.svg";
+import Gears from "../../../public/Group 33.svg";
 
 export default function AboutUs() {
   return (
-    <section className=" mt-64 md:mt-0  min-h-screen flex flex-col items-center px-6 md:px-52 py-10  mx-auto max-w-7xl" id='about'>
-   {/* <svg
+    <section
+      className=" mt-64 md:mt-0  min-h-screen flex flex-col items-center px-6 md:px-52 py-10  mx-auto max-w-7xl"
+      id="about"
+    >
+      {/* <svg
         className="absolute top-[60rem] left-[40rem] -translate-x-1/2 hidden md:block w-full h-full z-0"
         viewBox="0 0 800 800"
         fill="none"
@@ -27,7 +30,10 @@ export default function AboutUs() {
           <div className="md:w-1/2 text-left">
             <h2 className="text-3xl font-bold mb-5">WHO WE ARE</h2>
             <p className="">
-              We are a team of passionate innovators, problem-solvers, and technology enthusiasts dedicated to helping businesses thrive in the digital world. Our mission is to bridge the gap between business. and technology enthusiasts dedicated to helping businesses thrive in the digital world. Our mission is to bridge the gap between business.
+              We are a team of passionate innovators, problem-solvers, and
+              technology enthusiasts dedicated to helping businesses thrive in
+              the digital world. Our mission is to bridge the gap between
+              business.
             </p>
           </div>
           <div className="md:w-1/2 flex justify-center mt-4 md:mt-0">
@@ -40,11 +46,19 @@ export default function AboutUs() {
           <div className="md:w-1/2 text-left md:text-right">
             <h2 className="text-3xl font-bold mb-5">WHAT WE DO</h2>
             <p className="">
-              We are a team of passionate innovators, problem-solvers, and technology enthusiasts dedicated to helping businesses thrive in the digital world. Our mission is to bridge the gap between business.and technology enthusiasts dedicated to helping businesses thrive in the digital world. Our mission is to bridge the gap between business.
+              We are a team of passionate innovators, problem-solvers, and
+              technology enthusiasts dedicated to helping businesses thrive in
+              the digital world. Our mission is to bridge the gap between
+              business.
             </p>
           </div>
           <div className="md:w-1/2 flex justify-center mt-4 md:mt-0">
-            <Image src={Business} width={250} height={300} alt="Business Icon" />
+            <Image
+              src={Business}
+              width={250}
+              height={300}
+              alt="Business Icon"
+            />
           </div>
         </div>
 
@@ -53,7 +67,10 @@ export default function AboutUs() {
           <div className="md:w-1/2 text-left">
             <h2 className="text-3xl font-bold mb-5">WHAT MAKES US DIFFERENT</h2>
             <p className="">
-              We are a team of passionate innovators, problem-solvers, and technology enthusiasts dedicated to helping businesses thrive in the digital world. Our mission is to bridge the gap between business.
+              We are a team of passionate innovators, problem-solvers, and
+              technology enthusiasts dedicated to helping businesses thrive in
+              the digital world. Our mission is to bridge the gap between
+              business.
             </p>
           </div>
           <div className="md:w-1/2 flex justify-center mt-4 md:mt-0">
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState } from "react";
 import Link from "next/link";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
@@ -12,15 +12,33 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6 md:px-4 py-4">
         {/* Logo (Left Side) */}
         <div className="text-2xl font-bold text-primary order-1">
-          <Link href="/" className="text-[#563589] font-josephin">ShalOps.</Link>
+          <Link href="/" className="text-[#563589] font-josephin">
+            ShalOps.
+          </Link>
         </div>
 
         {/* Navigation Links - Desktop (Right Side) */}
         <ul className="hidden lg:flex space-x-6 items-center ml-auto order-2">
-          <li><a href="#hero" className="text-gray-700 hover:text-[#563589] ">Home</a></li>
-          <li><a href="#about" className="text-gray-700 hover:text-[#563589]">About Us</a></li>
-          <li><a href="#services" className="text-gray-700 hover:text-[#563589]">Services</a></li>
-          <li><a href="#portfolio" className="text-gray-700 hover:text-[#563589]">Portfolio</a></li>
+          <li>
+            <a href="#hero" className="text-gray-700 hover:text-[#563589] ">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="text-gray-700 hover:text-[#563589]">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="text-gray-700 hover:text-[#563589]">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" className="text-gray-700 hover:text-[#563589]">
+              Portfolio
+            </a>
+          </li>
           <li>
             <button className="px-6 py-3 bg-gradient-to-r from-[#8C52FF] to-[#160E23] text-white font-semibold rounded-lg shadow-md hover:opacity-90">
               Contact
@@ -41,10 +59,42 @@ const Navbar = () => {
       {isOpen && (
         <div className="lg:hidden absolute top-full left-0 w-full bg-[#f3eff9] shadow-md transition-all duration-300">
           <ul className="flex flex-col items-center space-y-4 py-6">
-            <li><a href="#hero" className="text-gray-700" onClick={() => setIsOpen(false)}>Home</a></li>
-            <li><a href="#about" className="text-gray-700" onClick={() => setIsOpen(false)}>About Us</a></li>
-            <li><a href="#services" className="text-gray-700" onClick={() => setIsOpen(false)}>Services</a></li>
-            <li><a href="#portfolio" className="text-gray-700" onClick={() => setIsOpen(false)}>Portfolio</a></li>
+            <li>
+              <a
+                href="#hero"
+                className="text-gray-700"
+                onClick={() => setIsOpen(false)}
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                className="text-gray-700"
+                onClick={() => setIsOpen(false)}
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="#services"
+                className="text-gray-700"
+                onClick={() => setIsOpen(false)}
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#portfolio"
+                className="text-gray-700"
+                onClick={() => setIsOpen(false)}
+              >
+                Portfolio
+              </a>
+            </li>
             <li>
               <button className="px-6 py-3 bg-gradient-to-r from-[#8C52FF] to-[#160E23] text-white font-semibold rounded-lg shadow-md hover:opacity-90">
                 Contact
